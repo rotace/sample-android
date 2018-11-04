2d073fcb71f6026e0c895f05ce63c550c704902c
Yasunori Shibata <rotace2012227ys@gmail.com>
2018-11-04 10:35:51 +0900

    modified tutorial google api

--- tutorialgoogleapi/src/main/java/com/example/tutorialgoogleapi/MainActivity.java
+++ tutorialgoogleapi/src/main/java/com/example/tutorialgoogleapi/MainActivity.java
@@ -10,8 +10,15 @@ import android.util.Log;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
 import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
 import com.google.android.gms.fitness.result.DataReadResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -20,84 +27,211 @@ import com.google.android.gms.tasks.Task;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     private static final int  GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
-    private static final String LOG_TAG = "Smart_Health";
+    private static final String TAG = "Smart_Health";
+    private DataSource mDataSource = null;
+    private OnDataPointListener mListener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(LOG_TAG, "FitnessOptions");
-        FitnessOptions fitnessOptions = FitnessOptions.builder()
-                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .build();
+        Log.d(TAG, "SignIn");
+        run();
+    }
 
-        Log.d(LOG_TAG, "SignIn");
-        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
-            Log.d(LOG_TAG, "requestPermissions");
-            GoogleSignIn.requestPermissions(
-                    this, // your activity
-                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                    GoogleSignIn.getLastSignedInAccount(this),
-                    fitnessOptions);
+    private void run(){
+        if (!hasOAuthPermission()) {
+            Log.d(TAG, "requestPermissions");
+            requestOAuthPermission();
         } else {
-            Log.d(LOG_TAG, "accessGoogleFit");
+            Log.d(TAG, "accessGoogleFit");
             accessGoogleFit();
         }
+    }
+
+    private FitnessOptions getFitnessSignInOptions() {
+        return FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.TYPE_WEIGHT, FitnessOptions.ACCESS_WRITE)
+                .build();
+    }
+
+    private boolean hasOAuthPermission() {
+        FitnessOptions fitnessOptions = getFitnessSignInOptions();
+        return GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions);
+    }
+
+    private void requestOAuthPermission() {
+        FitnessOptions fitnessOptions = getFitnessSignInOptions();
+        GoogleSignIn.requestPermissions(
+                this,
+                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                GoogleSignIn.getLastSignedInAccount(this),
+                fitnessOptions);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                accessGoogleFit();
+                run();
+//                accessGoogleFit();
             }
         } else {
             if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                Log.d(LOG_TAG, "Denied");
+                Log.d(TAG, "Denied");
             }
         }
     }
 
     private void accessGoogleFit() {
+
         Calendar cal = Calendar.getInstance();
         cal.setTime(new Date());
         long endTime = cal.getTimeInMillis();
         cal.add(Calendar.YEAR, -1);
         long startTime = cal.getTimeInMillis();
 
-        Log.d(LOG_TAG, "readRequest");
+        Log.d(TAG, "readRequest");
         DataReadRequest readRequest = new DataReadRequest.Builder()
                 .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
                 .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                 .bucketByTime(1, TimeUnit.DAYS) // deal with IllegalStateException
                 .build();
 
-        Log.d(LOG_TAG, "getHistoryClient");
+        Log.d(TAG, "getHistoryClient");
         Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
                 .readData(readRequest)
                 .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
                     @Override
                     public void onSuccess(DataReadResponse dataReadResponse) {
-                        Log.d(LOG_TAG, "onSuccess()");
+                        Log.d(TAG, "onSuccess()");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.e(LOG_TAG, "onFailure()", e);
+                        Log.e(TAG, "onFailure()", e);
                     }
                 })
                 .addOnCompleteListener(new OnCompleteListener<DataReadResponse>() {
                     @Override
                     public void onComplete(@NonNull Task<DataReadResponse> task) {
-                        Log.d(LOG_TAG, "onComplete()");
+                        Log.d(TAG, "onComplete()");
                     }
                 });
+
+        findDataSources();
+
     }
+
+    private void findDataSources() {
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .findDataSources(
+                        new DataSourcesRequest.Builder()
+                                .setDataTypes(
+                                        DataType.TYPE_WEIGHT
+                                )
+                                .setDataSourceTypes(DataSource.TYPE_RAW)
+                                .build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<List<DataSource>>() {
+                            @Override
+                            public void onSuccess(List<DataSource> dataSources) {
+                                for (DataSource dataSource : dataSources) {
+                                    Log.i(TAG, "Data source found: " + dataSource.toString());
+                                    Log.i(TAG, "Data Source type: " + dataSource.getDataType().getName());
+
+                                    if (dataSource.getDataType().equals(DataType.TYPE_WEIGHT)){
+                                        mDataSource = dataSource;
+                                    }
+                                    // Let's register a listener to receive Activity data!
+                                    if (dataSource.getDataType().equals(DataType.TYPE_WEIGHT)
+                                            && mListener == null) {
+                                        Log.i(TAG, "Data source for TYPE_WEIGHT found!  Registering.");
+                                        registerFitnessDataListener(dataSource, DataType.TYPE_WEIGHT);
+                                    }
+                                }
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "failed", e);
+                            }
+                        });
+    }
+
+    private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
+        // [START register_data_listener]
+        mListener =
+                new OnDataPointListener() {
+                    @Override
+                    public void onDataPoint(DataPoint dataPoint) {
+                        for (Field field : dataPoint.getDataType().getFields()) {
+                            Value val = dataPoint.getValue(field);
+                            Log.i(TAG, "Detected DataPoint field: " + field.getName());
+                            Log.i(TAG, "Detected DataPoint value: " + val);
+                        }
+                    }
+                };
+
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .add(
+                        new SensorRequest.Builder()
+                                .setDataSource(dataSource) // Optional but recommended for custom data sets.
+                                .setDataType(dataType) // Can't be omitted.
+//                                .setSamplingRate(10, TimeUnit.SECONDS)
+                                .build(),
+                        mListener)
+                .addOnCompleteListener(
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.i(TAG, "Listener registered!");
+                                } else {
+                                    Log.e(TAG, "Listener not registered.", task.getException());
+                                }
+                            }
+                        });
+        // [END register_data_listener]
+    }
+
+    /** Unregisters the listener with the Sensors API. */
+    private void unregisterFitnessDataListener() {
+        if (mListener == null) {
+            // This code only activates one listener at a time.  If there's no listener, there's
+            // nothing to unregister.
+            return;
+        }
+
+        // [START unregister_data_listener]
+        // Waiting isn't actually necessary as the unregister call will complete regardless,
+        // even if called from within onStop, but a callback can still be added in order to
+        // inspect the results.
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .remove(mListener)
+                .addOnCompleteListener(
+                        new OnCompleteListener<Boolean>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Boolean> task) {
+                                if (task.isSuccessful() && task.getResult()) {
+                                    Log.i(TAG, "Listener was removed!");
+                                } else {
+                                    Log.i(TAG, "Listener was not removed.");
+                                }
+                            }
+                        });
+        // [END unregister_data_listener]
+    }
+    
 }
